0cd6f53f18a78c7c9fdc0a13c81ac700354b0a01
S.A.V.C <81919005+SAVC16@users.noreply.github.com>
2025-07-08 21:28:42 -0500

    Configuracion - Extencion de Formatos

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
-#Config file
-# Define default file type to folder mappings
-
 DEFAULT_FOLDERS = {
-    'Music': ['mp3', 'wav', 'flac', 'ogg', 'aac'],
-    'Photos': ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff'],
-    'Docs': ['txt', 'pdf', 'docx', 'xlsx', 'pptx', 'csv'],
-    'Videos': ['mp4', 'mkv', 'mov', 'avi', 'wmv', 'flv']
+    'Music': ['mp3', 'wav', 'flac', 'ogg', 'aac', 'wma', 'opus','acc'],
+    'Photos': ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp', 'svg', 'raw', 'heic','eps'],
+    'Docs': ['txt', 'pdf', 'docx', 'xlsx', 'pptx', 'csv', 'odt', 'rtf', 'md', 'epub','psd'],
+    'Videos': ['mp4', 'mkv', 'mov', 'avi', 'wmv', 'flv', 'webm', 'mpeg', '3gp', 'm4v'],
 }
